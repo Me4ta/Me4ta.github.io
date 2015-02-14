67c6cd669beb1271e023ac250cc5868320a54947
Restuta <restuta8@gmail.com>
2015-02-14 15:32:53 -0800

    added bunch of things

--- 2015/love/js/main.js
+++ 2015/love/js/main.js
@@ -1,13 +1,25 @@
 (function(){
-    var doEvery = function(){
-        console.log(moment().hours());
+
+    var startDateTime = moment([2015, 01, 14, 10, 0, 0]);
+    var halfAnHoursPassed = 13;
+
+    var getMinutesPassed = function() {    
+        var difInMinutes = moment().diff(startDateTime, 'minutes');
+        return difInMinutes;
     }
 
-    $(function() {
-        setInterval(doEvery, 1000);
-        var i = 1;
-        $('#do').click(function () {
-            i++;
+
+    var doEvery = function(){
+        // halfAnHoursPassed = Math.floor(getMinutesPassed() / 30);
+        // console.log(halfAnHoursPassed);
+    }
+
+    var updateCurrentTo = function(i) {
+            console.log(i);
+            if (i == 1) {
+                $('.heart.1').addClass('current');
+                $('.lists li.1').addClass('current');
+            }
 
             if (i == 14) {
                 $('.heart.current').removeClass('current').addClass('past');
@@ -27,6 +39,19 @@
 
             $('.lists li.current').removeClass('current').addClass('past');
             $('.lists li.' + i).addClass('current');
+    }
+
+    $(function() {
+        setInterval(doEvery, 1000);
+
+        var i;
+        for (i = 1; i <= halfAnHoursPassed; i++) {
+            updateCurrentTo(i);
+        }
+
+        $('#do').click(function () {
+            updateCurrentTo(i);
+            i++;
         })
     });
 })();
