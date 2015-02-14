50eca7291814c5f47eaa991aa5f92444a45fb429
Restuta <restuta8@gmail.com>
2015-02-14 11:37:01 -0800

    added animation

--- 2015/love/js/main.js
+++ 2015/love/js/main.js
@@ -5,8 +5,28 @@
 
     $(function() {
         setInterval(doEvery, 1000);
+        var i = 1;
         $('#do').click(function () {
-            console.log('clicked');
+            i++;
+
+            if (i == 14) {
+                $('.heart.current').removeClass('current').addClass('past');
+
+                $('.lists li.current').removeClass('current').addClass('past');
+                $('.lists li.' + i).addClass('current');
+
+                $('.heart-big-wrapper').addClass('current');
+                $('.heart.past').addClass('final');
+
+                i = 1;
+                return;
+            }
+
+            $('.heart.current').removeClass('current').addClass('past');
+            $('.heart.'+ i).addClass('current');
+
+            $('.lists li.current').removeClass('current').addClass('past');
+            $('.lists li.' + i).addClass('current');
         })
     });
 })();
