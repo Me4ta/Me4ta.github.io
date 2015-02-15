a95f8667cde95ef13f4b939d6c6769ff77f87a2d
Restuta <restuta8@gmail.com>
2015-02-14 18:07:26 -0800

    added everything

--- 2015/love/js/main.js
+++ 2015/love/js/main.js
@@ -1,23 +1,43 @@
 (function(){
 
-    var startDateTime = moment([2015, 01, 14, 15, 43, 0]);
-    var intervalDurationMin = 1;
-    var intervalsPassed = 1;
+    var startDateTime = moment([2015, 01, 14, 18, 20, 0]);
+    var intervalDurationMin = 6;
+    var intervalId;
+    
 
     var getMinutesPassed = function() {    
         var difInMinutes = moment().diff(startDateTime, 'minutes');
         return difInMinutes;
     }
 
+    var getIntervalsPassed = function() {
+        return Math.floor(getMinutesPassed() / intervalDurationMin);
+    }
+
+    var intervalsPassed = getIntervalsPassed();;
 
     var doEvery = function(){
-        //intervalsPassed = Math.floor(getMinutesPassed() / intervalDurationMin);
         
-        var minutesPassedFromStart = moment().diff(startDateTime, 'seconds');
-        var momentDuration = moment.duration(minutesPassedFromStart, 'seconds');
+        if (getMinutesPassed() < 0) {
+            //var startingIn = moment.duration();
+            $('.time-left').text('Starting at ' + startDateTime.format('hh:mm a') + '...') ;
+            return;
+        }
+
+        intervalsPassed = getIntervalsPassed() + 1;
+        
+        var secondsPassedFromStart = moment().diff(startDateTime, 'seconds');
+        var durationPassed = moment.duration(intervalDurationMin * intervalsPassed, 'minutes');
+        var timeLeftToNext = durationPassed.asSeconds() - secondsPassedFromStart;
+        var momentDuration = moment.duration(timeLeftToNext, 'seconds');
+        
         
         $('.time-left').text(momentDuration.minutes() + ' min ' 
             + momentDuration.seconds() + ' sec ');
+
+        if (momentDuration.minutes() == 0 && momentDuration.seconds() == 1) {
+            updateCurrentTo(intervalsPassed);
+        }
     }
 
     var updateCurrentTo = function(i) {
@@ -25,6 +45,7 @@
             if (i == 1) {
                 $('.heart.1').addClass('current');
                 $('.lists li.1').addClass('current');
+                return;
             }
 
             if (i == 14) {
@@ -35,6 +56,9 @@
 
                 $('.heart-big-wrapper').addClass('current');
                 $('.heart.past').addClass('final');
+
+                $('.time-left').hide();
+                clearInterval(intervalId);
 
                 i = 1;
                 return;
@@ -48,17 +72,14 @@
     }
 
     $(function() {
-        setInterval(doEvery, 1000);
-
         var i;
         for (i = 1; i <= intervalsPassed; i++) {
             updateCurrentTo(i);
         }
 
-        $('#do').click(function () {
-            updateCurrentTo(i);
-            i++;
-        })
+        if (intervalsPassed < 14) {
+            intervalId = setInterval(doEvery, 1000);
+        }
     });
 })();
 
