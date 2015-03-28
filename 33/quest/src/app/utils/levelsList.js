d3fb58a50b311355cc9bcb0ab740271e7b421a33
Restuta <restuta8@gmail.com>
2015-03-28 15:00:56 -0700

    final

--- 33/quest/src/app/utils/levelsList.js
+++ 33/quest/src/app/utils/levelsList.js
@@ -19,29 +19,28 @@ export default Ember.A([
   {number: 12, name: 'Todo', code: 'BUOY'},
   {number: 13, name: 'Just like grass', code: 'RRAM'},
   {number: 14, name: 'Programming classic', code: 'ITRE'},
-
   {number: 15, name: 'It\'s math, bitches!', code: 'АКЬЛ'},
   {number: 16, name: 'Oh no, not again!', code: 'CARS'},
 
   {number: 17, name: 'All the places', code: 'AWEB'},
-  {number: 18, name: 'First', code: 'RTEK'},
-  {number: 19, name: 'First', code: 'EFFO'},
-  {number: 20, name: 'First', code: 'WYBA'},
-  {number: 21, name: 'First', code: 'SERY'},
-  {number: 22, name: 'First', code: 'M?EM'},
-  {number: 23, name: 'First', code: 'CINU'},
-  {number: 24, name: 'First', code: 'VGNI'},
+  {number: 18, name: 'Double-edged', code: 'RTEK'},
+  {number: 19, name: 'Smile', code: 'EFFO'},
+  {number: 20, name: 'Good bye, cruel world...', code: 'WYBA'},
+  {number: 21, name: 'Such a cute robot', code: 'SERY'},
+  {number: 22, name: 'Catch the moment', code: 'M?EM'},
+  {number: 23, name: 'Animals everywhere', code: 'CINU'},
+  {number: 24, name: 'Hard Puzzle', code: 'VGNI'},
 
-  {number: 25, name: 'First', code: 'EILL'},
-  {number: 26, name: 'First', code: 'LEVA'},
-  {number: 27, name: 'First', code: 'SERE'},
-  {number: 28, name: 'First', code: 'TENI'},
-  {number: 29, name: 'First', code: 'TCEP'},
-  {number: 30, name: 'First', code: 'CISU'},
-  {number: 31, name: 'First', code: '8NRO'},
-  {number: 32, name: 'First', code: 'SAGE'},
+  {number: 25, name: 'Was not so hard? Ok!', code: 'EILL'},
+  {number: 26, name: 'Pizza Delivery!', code: 'LEVA'},
+  {number: 27, name: 'Plenty of fish', code: 'SERE'},
+  {number: 28, name: 'Peace', code: 'TENI'},
+  {number: 29, name: 'Blue or Red?', code: 'TCEP'},
+  {number: 30, name: 'When I grow up...', code: 'CISU'},
+  {number: 31, name: 'Pink Fluffy Unicorns', code: '8NRO'},
+  {number: 32, name: 'Naruto?', code: 'SAGE'},
 
-  {number: 33, name: 'First', code: 'LOVE'}
+  {number: 33, name: '♡♡♡♡♡♡♡♡', code: 'LOVE'}
 ]);
 
 
